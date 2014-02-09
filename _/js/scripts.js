7990408822f3f7e688584e7a7d21c174b4ef558c
nurogenic <don.desroches@gmail.com>
2014-02-09 12:00:20 -0500

    updating app to add pattern

--- _/js/scripts.js
+++ _/js/scripts.js
@@ -4,65 +4,65 @@
 		Models: {},
 		Collections: {},
 		Views: {},
-		Router: {}
+		Router: {},
+		Helpers: {}
 	};
 
-	window.template = function(id){
-		return _.template( $( '#' + id ).html() );
-	}
+	var bEvents = _.extend({}, Backbone.Events);
 
+	DesignPatterns.Helpers = {
+		
+		template: function(id){
+			return _.template( $( '#' + id ).html() );
+		},
 
-	var events = _.extend({}, Backbone.Events);
+		updateBodyClass: function(newClass){
+			$('body').attr('class','');
+			$('body').addClass(newClass);
+		},
+
+		incrementId: function(newPattern){
+			var patternCount = patternCollection.toJSON().length;
+			newPattern.model.set('id', patternCount);
+		},
+
+		alertError: function(error){
+			alert(error);
+		}
+	};
 
 	// Handles the rendering of different routes
 	DesignPatterns.Router = Backbone.Router.extend({
 		routes: {
 	      ''				: 'list',
-	      'list'			: 'list',
-	      'details/:id'		: 'details',
-	      'search/:query' 	: 'search',
+	      'list(/)'			: 'list',
+	      'details(/):id'	: 'details',
+	      'add(/)'			: 'add',
+	      'search(/):query'	: 'search',
 	      '*other'			: 'default'
 	    },
 
 	    list: function(){
-			
-			$('#page-title').html('Learning Design Patterns');
-
-			var template = window.template( 'list-view-container' );
-			$('#header').html( template({title: 'Javascript Design Patterns'}) );
-
-			$("#design-pattern-content").html('');
-			$("#design-pattern-content").html(patternView);
-
-			$('body').attr('class','');
-			$('body').addClass('list-view');
-
-			$('#back-button').hide();
+			var thisTemplate = new DesignPatterns.Views.ShowPatterns();
+	    	bEvents.trigger('patternTemplate:all');
 		},
 
 	    details: function(id){
-	    	var model = patternCollection.get(id);
+	    	var thisTemplate = new DesignPatterns.Views.ShowPattern();
+	    	bEvents.trigger('patternTemplate:show', id);
+	    },
 
-	    	var template = window.template( 'list-view-container' );
-	    	$('#header').html( template( {title : model.get('title') }) );
-
-	    	$("#design-pattern-content").html( model.get('description') );
-	    	$('body').attr('class','');
-			$('body').addClass('details-view');
-			$('#back-button').show();
+	    add: function(){
+	    	var thisTemplate = new DesignPatterns.Views.AddPattern();
+	    	bEvents.trigger('patternTemplate:add');
 	    },
 
 	    search: function(){
-	    	
 	    },
 
 	    default: function(other){
-
-	    	$('#page-title').html("The page at "+other+" doesn't exist.");
-	    	$("#design-pattern-content").html('');
-	    	$('body').attr('class','');
-			$('body').addClass('error-page');
-			$('#back-button').hide();
+	    	var thisTemplate = new DesignPatterns.Views.Error();
+	    	bEvents.trigger('patternTemplate:error', other);
 	    }
 	});
 
@@ -72,7 +72,9 @@
 			id : 0,
 			title : '',
 			description : ''
-		}
+		},
+
+
 	});
 
 	DesignPatterns.Collections.Patterns = Backbone.Collection.extend({
@@ -83,12 +85,24 @@
 	DesignPatterns.Views.Patterns = Backbone.View.extend({
 		tagName : 'ul',
 
+		initialize: function(){
+			this.collection.on('add', this.addOne, this);
+		},
+
 		render: function(){
 			this.collection.each( function(pattern){
 				var patternView = new DesignPatterns.Views.Pattern({model: pattern});
 				this.$el.append(patternView.render().el);
 			}, this);
 			return this;
+		},
+
+		addOne: function(pattern){
+			var newPattern = new DesignPatterns.Views.Pattern({ model: pattern});
+
+			DesignPatterns.Helpers.incrementId(newPattern);
+
+			this.$el.append( newPattern.render().el );
 		}
 	});
 
@@ -96,7 +110,7 @@
 	DesignPatterns.Views.Pattern = Backbone.View.extend({
 		tagName : "li",
 
-		template: template('pattern-template'),
+		template: DesignPatterns.Helpers.template('pattern-template'),
 
 		render: function(){
 			this.$el.html( this.template( this.model.toJSON() ) );
@@ -104,13 +118,127 @@
 		}
 	});
 
+	// Handles adding new pattern
+	DesignPatterns.Views.AddPattern = Backbone.View.extend({
+		tagName: 'form',
+
+		id: 'add-new-pattern',
+
+		template: DesignPatterns.Helpers.template('add-one'),
+
+		events: {
+			'submit' : 'submit'
+		},
+
+		initialize: function(){
+			bEvents.on('patternTemplate:add', this.render, this);
+		},
+
+		render: function(){
+			var template = DesignPatterns.Helpers.template( 'list-view-container' );
+	    	$('#header').html( template( {title : 'Add A New Design Pattern' }) );
+
+	    	$("#design-pattern-content").html( this.$el.html(this.template) );
+
+	    	DesignPatterns.Helpers.updateBodyClass('add-new');
+
+			$('#back-button').show();
+			$('#add-button').hide();
+			return this;
+		},
+
+		submit: function(e){
+			e.preventDefault();
+
+			var inputTitle 			= $('input[name="design-pattern-name"]');
+			var inputDescription 	= $('textarea[name="design-pattern-description"]');
+
+			var patternTitle = inputTitle.val();
+			var patternDescription = inputDescription.val();
+
+			if(patternTitle !== '' && patternDescription !== ''){
+				inputTitle.val('');
+				inputDescription.val('').html('');
+				patternCollection.add( { title: patternTitle , description: patternDescription } );
+				router.navigate("list", {trigger: true});
+			} else{
+				DesignPatterns.Helpers.alertError("Please fill out all the information.");
+			}
+
+		}
+	});
+
+	// Handles showing specific pattern details
+	DesignPatterns.Views.ShowPattern = Backbone.View.extend({
+
+		template: DesignPatterns.Helpers.template( 'list-view-container' ),
+
+		initialize: function(){
+			bEvents.on('patternTemplate:show', this.render, this)
+		},
+
+		render: function(id){
+			var model = patternCollection.get(id);
+			$("#header").html( this.$el.html(this.template({title : model.get('title')})) );
+			$("#design-pattern-content").html( model.get('description') );
+			
+			DesignPatterns.Helpers.updateBodyClass('details-view');
+
+			$('#back-button').show();
+			$('#add-button').hide();
+			return this;
+		}
+	});
+
+	// Handles showing all patterns
+	DesignPatterns.Views.ShowPatterns = Backbone.View.extend({
+
+		template: DesignPatterns.Helpers.template( 'list-view-container' ),
+
+		initialize: function(){
+			bEvents.on('patternTemplate:all', this.render, this)
+		},
+
+		render: function(){
+			$("#header").html( this.$el.html(this.template({title: 'Javascript Design Patterns'})) );
+			$("#design-pattern-content").html(patternView);
+
+			DesignPatterns.Helpers.updateBodyClass('list-view');
+
+			$('#back-button').hide();
+			$('#add-button').show();
+
+			return this;
+		}	
+	});
+
+	DesignPatterns.Views.Error = Backbone.View.extend({
+		template: DesignPatterns.Helpers.template( 'list-view-container' ),
+
+		initialize: function(){
+			bEvents.on('patternTemplate:error', this.render, this)
+		},
+
+		render: function(other){
+
+			$("#header").html( this.$el.html(this.template({title: 'The page at "'+other+'" doesn\'t exist.'})) );
+
+	    	$("#design-pattern-content").html('');
+			
+			DesignPatterns.Helpers.updateBodyClass('error-page');
+
+			$('#back-button').show();
+			$('#add-button').hide();
+
+			return this;
+		}
+	});
 
 	//gather my collection and render
 	var patternCollection 	= new DesignPatterns.Collections.Patterns( window.patternArray );
 	var patternView 		= new DesignPatterns.Views.Patterns( {collection: patternCollection} ).render().el;
 
-
 	// start my routers
-	new DesignPatterns.Router;
+	var router = new DesignPatterns.Router;
 	Backbone.history.start();
 })();
